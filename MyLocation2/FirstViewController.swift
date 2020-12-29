2bf3f5094b8bbd9dbdcb5fe40db8aca1bb476e65
Edward Tang <edwardtang@Edwards-MacBook-Pro.local>
2020-12-28 18:46:45 -0800

    start

---
+++ MyLocation2/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyLocation2
+//
+//  Created by Edward Tang on 12/28/20.
+//  Copyright © 2020 Edward Tang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
